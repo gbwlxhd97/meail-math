132a08a85540977bed7ae48106fc07873773fdd9
gbwlxhd97 <gbwlxhd97@naver.com>
2021-11-24 22:57:38 +0900

    create controller && router dir

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ import 'express-async-errors'
 import cors from "cors";
 import morgan from 'morgan';
 import helmet from 'helmet';
-
+import AuthRouter from "./router/auth.js";
+import TimerRouter from "./router/timer.js";
 
 const app = express();
-app.use(express.json());
-app.use(helmet());
-app.use(cors());
-app.use(morgan('tiny'));
+app.use(express.json()); // post data를 json처리해줘서 읽을 수 있음
+app.use(helmet()); // 보안 header파일을 모두 사용하게해줌.
+app.use(cors()); // import use만으로 cors 정책 허가 
+app.use(morgan('tiny')); // req가 들어오면 log로 찍어서 남길 수 있음 인자 tiny는 format 형식임.
 
-// app.use('') 라우터처리.
+app.use('/signUp' ,AuthRouter) //라우터처리.
+app.use('/timer' ,TimerRouter) 
 
 app.use((req,res,next) => {
     res.sendStatus(404) 
