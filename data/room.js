36a49677098fc2921b997969083eb3231ff4f53d
gbwlxhd97 <gbwlxhd97@naver.com>
2021-12-08 00:15:01 +0900

    방 입장 퇴장 구현

--- data/room.js
+++ data/room.js
@@ -4,7 +4,7 @@ let rooms = [
         title: "1번방",
         subject: "국어",
         info: "인포메이션",
-        participants: 0
+        participants: 1
     }
 ]
 
@@ -15,15 +15,25 @@ let room = [
 export async function findRooms() {
     return rooms;
 }
-
+export async function findRoom() {
+    return room;
+}
 export async function createRoom(room) {
     const created = {id: Date.now().toString(),...room};
     rooms.push(created);
     return created.id
 }
 
-export async function enterRoom(name) {
-    const created = {...name};
+export async function enterRoom(user) {
+    const created = {...user};
     room.push(created);
     return created.title;
+}
+
+export async function exitRoom(user) {
+    const exit = {...user};
+    //임시로 이름으로함
+    let index = room.findIndex(item => item.name === exit.name)
+    room.splice(index,1);
+    return `${exit.name}님이 퇴장하셨습니다.`
 }
